afd73897f996ca1a27902387d78d0fadb15db742
ssy42 <ssy4230@naver.com>
2020-04-24 23:57:54 +0900

    lambda환경설정, 데이터이름은 바꿀예정

--- app/src/main/java/com/example/cooperativeproject2/MainActivity.java
+++ app/src/main/java/com/example/cooperativeproject2/MainActivity.java
@@ -6,11 +6,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import com.amazonaws.mobileconnectors.lambdainvoker.*;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TableLayout;
+import android.widget.Toast;
 
 import com.example.cooperativeproject2.Adapter.FragmentAdapter;
+import com.example.cooperativeproject2.Lambdaeventgenerator.MyInterface;
+import com.example.cooperativeproject2.Lambdaeventgenerator.RequestClass;
+import com.example.cooperativeproject2.Lambdaeventgenerator.ResponseClass;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
@@ -33,6 +43,47 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(fragmentAdapter);
 
         tabLayout.setupWithViewPager(viewPager);
+
+        //lambda용 코드
+        // Create an instance of CognitoCachingCredentialsProvider
+        CognitoCachingCredentialsProvider cognitoProvider = new CognitoCachingCredentialsProvider(
+                this.getApplicationContext(), "ap-northeast-2:b8705257-f808-45df-b68a-364b5366f7db", Regions.AP_NORTHEAST_2);
+
+// Create LambdaInvokerFactory, to be used to instantiate the Lambda proxy.
+        LambdaInvokerFactory factory = new LambdaInvokerFactory(this.getApplicationContext(),
+                Regions.AP_NORTHEAST_2, cognitoProvider);
+
+// Create the Lambda proxy object with a default Json data binder.
+// You can provide your own data binder by implementing
+// LambdaDataBinder.
+        final MyInterface myInterface = factory.build(MyInterface.class);
+
+        RequestClass request = new RequestClass("John", "Doe");
+// The Lambda function invocation results in a network call.
+// Make sure it is not called from the main thread.
+        new AsyncTask<RequestClass, Void, ResponseClass>() {
+            @Override
+            protected ResponseClass doInBackground(RequestClass... params) {
+                // invoke "echo" method. In case it fails, it will throw a
+                // LambdaFunctionException.
+                try {
+                    return myInterface.AndroidBackendLambdaFunction(params[0]);
+                } catch (LambdaFunctionException lfe) {
+                    Log.e("Tag", "Failed to invoke echo", lfe);
+                    return null;
+                }
+            }
+
+            @Override
+            protected void onPostExecute(ResponseClass result) {
+                if (result == null) {
+                    return;
+                }
+
+                // Do a toast
+                Toast.makeText(MainActivity.this, result.getGreetings(), Toast.LENGTH_LONG).show();
+            }
+        }.execute(request);
 
     }
 }

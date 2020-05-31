b5897b2116234bcfad7b3532c608028359ff0edd
Mokyung Kim <monee1001@naver.com>
2020-06-01 02:23:57 +0900

    아이콘(대충), 로딩화면

---
+++ app/src/main/java/com/example/cooperativeproject2/LoadingActivity.java
@@ -0,0 +1,30 @@
+package com.example.cooperativeproject2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading); //xml , java 소스 연결
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(intent); //다음화면으로 넘어감
+                finish();
+            }
+        },3000); //3초 뒤에 Runner객체 실행하도록 함
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
